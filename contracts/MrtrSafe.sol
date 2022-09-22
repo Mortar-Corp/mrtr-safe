42fa294cd542f47fec19c161d443eacf46570c34
SashaFlores <sashaflores207@gmail.com>
2022-09-22 14:20:15 -0400

    VCT&Adjust

--- contracts/MrtrSafe.sol
+++ contracts/MrtrSafe.sol
@@ -4,94 +4,66 @@ pragma solidity >=0.8.0 <0.9.0;
 import "./ReceiveBrick.sol";
 import "./Owners.sol";
 import "./Proxy/Initializable.sol";
-import "./Cryptography/ECDSAUpgradeable.sol";
-import "./Cryptography/draft-EIP712Upgradeable.sol";
 import "./Interfaces/IERC1155Modified.sol";
 import "./Token/ERC1155HolderModified.sol";
+import "./Interfaces/IERC20Upgradeable.sol";
 
 
 contract MrtrSafe is 
     Initializable,
     Owners, 
     ERC1155HolderModified, 
-    EIP712Upgradeable, 
     ReceiveBrick
 {
    
 
     IERC1155Modified private VCT;
+    IERC20Upgradeable private AND;
     uint256 private nonce;
     mapping(bytes32 => bool) private executed;
 
 
-    bytes32 private constant WITHDRAW_TYPEHASH = keccak256("Withdraw(string symbol,uint256 amount,uint256 nonce");
+  
 
     function __MrtrSafe_init(address[] memory _owners, uint256 _minApprovals) public payable initializer {
         __Owners_init(_owners, _minApprovals);
-        __EIP712_init("MrtrSafe", "1.0.0");
         __ERC1155HolderModified_init();
 
-        VCT = IERC1155Modified(0xC265Ee3c7173818dad8e212197044Eb3b23b55C9);
+        VCT = IERC1155Modified(0x0C12780426024405E90b9b40DEE4B8F99B2E3Da5);
+        //this is the stable coin address which doesn't exist yet
+        AND = IERC20Upgradeable(0x50Dd14Aa06f0032993E6a96fB314596BeccD25c4);
     }
 
 
+    //checks if VCT exists in safe if it's minted to safe not to address
     function VCTExist(uint256 id) public view returns(uint256) {
         return VCT.balanceOf(address(this), id);
     }
 
 
-    //BRICK balance needs testing
-    function walletBrickBalance() public view virtual returns(uint256) {
+    //checks BRCK balance per safe not per address
+    function BRCKBalance() public view virtual returns(uint256) {
         return address(this).balance;
     }
 
-    function withdraw(string calldata symbol, uint256 amount, bytes[] calldata signatures) public virtual onlyOwners {
-        require(verifySiganture(hashWithdraw(symbol, amount, nonce), signatures), "invalid siganture");
+
+    function transfer(address payable receiver, string calldata symbol, uint256 id, uint256 amount) public virtual onlyOwners {
+    
         if(keccak256(bytes(symbol)) == keccak256(bytes("BRCK"))) {
-            require(address(this).balance >= amount, "exceeds balance");
-            payable(msg.sender).transfer(amount);
+            require(address(this).balance >= amount, "Safe: exceeds available Brick's balance");
+            (bool success, ) = receiver.call{value: amount}("");
+            require(success, "Safe: transfer brick failed");
+            //no stable coin yet
+        } else if(keccak256(bytes(symbol)) == keccak256(bytes("AND"))) {
+            require(AND.balanceOf(address(this)) >= amount, "Safe: exceeds available AND's balance");
+        } else if(keccak256(bytes(symbol)) == keccak256(bytes("VCT"))) {
+            require(VCT.balanceOf(address(this), id) >= amount, "Safe: exceeds available VCT's balance");
+            VCT.safeTransferFrom(address(this), receiver, id, amount, "");
         }
-        (bool success, ) = msg.sender.call{value: amount}("");
-        require(success, "tx failed");
-        nonce++;
+        
     }
 
-    function hashWithdraw(string memory symbol, uint256 amount, uint256 _nonce) public view returns(bytes32) {
-        //bytes32 structHash = keccak256(abi.encode(WITHDRAW_TYPEHASH, keccak256(bytes(symbol)), amount, _nonce));
-        return _hashTypedDataV4(keccak256(abi.encode(
-            WITHDRAW_TYPEHASH,
-            keccak256(bytes(symbol)),
-            keccak256(abi.encodePacked(amount)),
-            keccak256(abi.encodePacked(_nonce))
-            )
-        ));
-    }
-
-    function verifySiganture(bytes32 hash, bytes[] memory signatures) public view returns(bool) {
-       
-        for(uint256 i = 0; i < signatures.length; i++) {
-            address signer = ECDSAUpgradeable.recover(hash, signatures[i]);
-            signer = owners[i];
-            bool valid = signer == owners[i];
-            if(!valid) {
-                return false;
-            }
-        }
-        return true;
-    }
-
-    function getChainId() external view returns (uint256) {
-        uint256 id;
-
-        assembly {
-            id := chainid()
-        }
-        return id;
-    }
-
-    function domainSeparator() external view returns(bytes32) {
-        return _domainSeparatorV4();
-    }
 }
+
 
 
